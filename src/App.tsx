37c56dcafc3caf3f9bc82d6048e839ae55e4b713
Mariana Proniuk <meriproo@gmail.com>
2023-04-21 19:44:10 +0300

    feat(App): fetch posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { api } from "./api";
+import { PostItem } from "./shared/components/ui";
+import { PostItemType } from "./shared/types";
+import { Navigation, Scrollbar } from "swiper";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [posts, setPosts] = useState<null | PostItemType[]>(null);
+
+	useEffect(() => {
+		api.postService.getAllPosts().then((result) => setPosts(result));
+	}, []);
+
+	if (!posts) return null;
+
+	return (
+		<Swiper
+			modules={[Navigation, Scrollbar]}
+			slidesPerView={3}
+			scrollbar={{ draggable: true }}
+		>
+			{posts.map((i) => (
+				<SwiperSlide key={i.id}>
+					<PostItem data={i} />
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
 }
 
 export default App;
