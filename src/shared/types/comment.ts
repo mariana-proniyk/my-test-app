0d14149bf25a61d6d6e01e16a2f23c43609f3ab5
Mariana Proniuk <meriproo@gmail.com>
2023-04-21 19:41:11 +0300

    feat: create types

---
+++ src/shared/types/comment.ts
@@ -0,0 +1,7 @@
+export type CommentItemType = {
+	postId: number;
+	id: number;
+	name: string;
+	email: string;
+	body: string;
+};
---
+++ src/shared/types/post.ts
@@ -0,0 +1,6 @@
+export type PostItemType = {
+	userId: number;
+	id: number;
+	title: string;
+	body: string;
+};
